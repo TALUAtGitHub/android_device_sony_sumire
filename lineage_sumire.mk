268dec00b427f33e586908317b32c7953c2ced67
TALU <taluemail272@gmail.com>
2020-12-22 13:45:27 +0000

    sumire: set proper and newer build fingerprints and descriptions and remove full_sumire.mk.

--- lineage_sumire.mk
+++ lineage_sumire.mk
@@ -14,9 +14,31 @@
 # limitations under the License.
 #
 
-$(call inherit-product, device/sony/sumire/full_sumire.mk)
+# Inherit device configuration
+$(call inherit-product, device/sony/sumire/device.mk)
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common LineageOS stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+PRODUCT_GMS_CLIENTID_BASE := android-sonymobile
+
+# Set those variables here to overwrite the inherited values.
 PRODUCT_NAME := lineage_sumire
+PRODUCT_BRAND := Sony
+PRODUCT_MANUFACTURER := Sony
+PRODUCT_DEVICE := sumire
+PRODUCT_MODEL := Xperia Z5
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE="sumire" \
+    PRODUCT_NAME="sumire" \
+    PRIVATE_BUILD_DESC="sumire-user 7.1.1 N-MR1-KITAKAMI-170920-0229 1 dev-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := Sony/sumire/sumire:7.1.1/N-MR1-KITAKAMI-170920-0229/1:user/dev-keys
+
+TARGET_VENDOR := Sony
